613d8bef63ed2caeecc188f377fb0605dc515805
Satyam Banwale <satyambanwale786@gmail.com>
2025-03-08 23:55:55 +0530

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
 import { AppModule } from './app.module'
-import * as cors from 'cors'
+import cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
